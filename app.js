6eaa02cd6b925f3e2f33d0e34f26407c42c8ffd7
hr280 <hr280161@gmail.com>
2020-07-10 23:43:33 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,50 @@
+// var min = 00;
+// var sec = 00;
+// var msec = 00;
+// var minHeading = document.getElementById("min");            
+// var secHeading = document.getElementById("sec") ;             
+// var msecHeading = document.getElementById("msec"); 
+// var interval;
+
+// function timer(){
+//   msec++
+//   msecHeading.innerHTML = msec;
+//   if(msec >= 100){
+//       sec++
+//       secHeading.innerHTML = sec;
+//       msec = 00;
+//   }else if(sec >= 60){
+//       min++
+//       sec = 00;
+//       minHeading.innerHTML = min;
+//   }
+// }
+// function start(){
+
+
+// interval = setInterval(timer , 10)
+// }
+// function stop(){
+//    clearInterval(interval)
+
+// }
+// function reset(){
+//  min = 00;
+//  sec = 00;
+//  msec = 00;
+//  minHeading.innerHTML = min
+//  secHeading.innerHTML = sec
+//  msecHeading.innerHTML = msec
+//  clearInterval(interval)
+ 
+// }
+
+
+
+
+
+
+
+
+
+
